a57ab4c95d427aabaf0d94f3973b34c3f5e45839
Manoj Kumar Patra <manoj1993patra@gmail.com>
2026-04-14 07:26:38 +0000

    New Terraform files added to repo

---
+++ terraform/env/prod.tfvars
@@ -0,0 +1,12 @@
+resource_group_name  = "rg-prod-001"
+location             = "East US"
+
+vm_name              = "vm-prod-app"
+admin_username       = "azureuser"
+
+storage_account_name = "stgprod001"
+
+sql_server_name      = "sql-prod-001"
+sql_db_name          = "appdb"
+sql_admin_user       = "sqladmin"
+sql_admin_password   = "REPLACE_AT_RUNTIME"
---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-prod"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_mssql_server" "sql" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = var.location
+  administrator_login          = var.sql_admin_user
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "sqldb" {
+  name      = var.sql_db_name
+  server_id = azurerm_mssql_server.sql.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "law-prod"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+}
