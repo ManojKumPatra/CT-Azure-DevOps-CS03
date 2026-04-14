9defc38e05f6e27575e2690699a84c309d7be9a1
Manoj Kumar Patra <manoj1993patra@gmail.com>
2026-04-14 07:36:46 +0000

    backend file modified.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-prod-001"
+    resource_group_name  = "rg-tfstate-001"
     storage_account_name = "tfstateprod001"
     container_name       = "tfstate"
     key                  = "iac.tfstate"
